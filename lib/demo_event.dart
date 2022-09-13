afc7043a1c13c7436868a597a66dd026c2fbd441
paladiyagithub <shreypaladiya6@gmail.com>
2022-09-13 09:43:30 +0530

    Initial commit

---
+++ lib/demo_event.dart
@@ -0,0 +1,19 @@
+part of 'demo_bloc.dart';
+
+@immutable
+abstract class DemoEvent {}
+class IntialEvent extends DemoEvent{
+  int i;
+  IntialEvent(int this.i);
+}
+
+
+class increment extends DemoEvent {
+
+
+}
+
+class Decremnt extends DemoEvent {
+
+
+}
